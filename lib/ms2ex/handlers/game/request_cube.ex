f92874133fc2ea7676ccc4e620549534b24a7a95
Stefano Gessa <stefano@gessa.net>
2021-09-22 12:45:06 +0700

    refactoring request cube handler

--- lib/ms2ex/handlers/game/request_cube.ex
+++ lib/ms2ex/handlers/game/request_cube.ex
@@ -1,5 +1,5 @@
 defmodule Ms2ex.GameHandlers.RequestCube do
-  alias Ms2ex.{Metadata, Packets, World}
+  alias Ms2ex.{Field, Metadata, Packets, World}
 
   import Packets.PacketReader
   import Ms2ex.Net.Session, only: [push: 2]
@@ -16,9 +16,8 @@ defmodule Ms2ex.GameHandlers.RequestCube do
     with {:ok, character} <- World.get_character(session.character_id),
          {:ok, map} <- Metadata.Maps.lookup(character.map_id),
          {:ok, object} <- find_object(map, coord) do
-      session
-      |> push(Packets.ResponseCube.pickup(character, object.weapon_id, coord))
-      |> push(Packets.UserBattle.set_stance(character, true))
+      Field.broadcast(character, Packets.UserBattle.set_stance(character, true))
+      push(session, Packets.ResponseCube.pickup(character, object.weapon_id, coord))
     else
       _ -> session
     end
@@ -27,18 +26,14 @@ defmodule Ms2ex.GameHandlers.RequestCube do
   # Drop
   def handle_mode(0x12, _packet, session) do
     with {:ok, character} <- World.get_character(session.character_id) do
-      session
-      |> push(Packets.ResponseCube.drop(character))
-      |> push(Packets.UserBattle.set_stance(character, false))
+      Field.broadcast(character, Packets.UserBattle.set_stance(character, false))
+      push(session, Packets.ResponseCube.drop(character))
     else
       _ -> session
     end
   end
 
   defp find_object(map, coord) do
-    IO.inspect(map.objects, label: "OBJ")
-    IO.inspect(coord, label: "COORD")
-
     case Enum.find(map.objects, &(&1.coord == coord)) do
       nil -> :error
       object -> {:ok, object}
