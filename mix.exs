bcd015824cf425a509672ec80faa52c4f51d182d
Stefano Gessa <stefano@gessa.net>
2022-12-04 13:23:21 +0700

    Upgrade `phoenix` to `1.6.15`

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Ms2ex.MixProject do
     [
       app: :ms2ex,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -33,12 +33,12 @@ defmodule Ms2ex.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5"},
-      {:phoenix_ecto, "~> 4.1"},
-      {:ecto_sql, "~> 3.4"},
-      {:postgrex, ">= 0.0.0"},
+      {:phoenix, "~> 1.6"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:ecto_sql, "~> 3.9"},
+      {:postgrex, "~> 0.16"},
       {:ecto_enum, ">= 0.0.0"},
-      {:phoenix_live_dashboard, "~> 0.4"},
+      {:phoenix_live_dashboard, "~> 0.7"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.0"},
