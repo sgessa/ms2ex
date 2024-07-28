744a3a30fbb321841dc9ce22769682c9f76db0d8
Alessandro Gessa <alessandro@gessa.net>
2024-07-28 21:28:55 +0200

    Update to Elixir 1.17 OTP 27

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Ms2ex.MixProject do
     [
       app: :ms2ex,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
