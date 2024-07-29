8e6ab0c0ca7864e1d917b9b36451efd8d5f10d80
Alessandro Gessa <alessandro@gessa.net>
2024-07-29 13:35:29 +0200

    Metadata.get/2 for multiple ids

--- lib/ms2ex/metadata.ex
+++ lib/ms2ex/metadata.ex
@@ -2,6 +2,10 @@ defmodule Ms2ex.Metadata do
   def get(struct, id) when is_struct(struct),
     do: get(struct.__struct__, id)
 
+  def get(module, ids) when is_list(ids) do
+    get(module, Enum.join(ids, "_"))
+  end
+
   def get(module, id) when is_atom(module) do
     prefix = Macro.underscore(module)
     key = "#{prefix}:#{id}"
