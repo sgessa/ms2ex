07377e8703265a6af2cd3971f0648d983cc30cfa
Stefano Gessa <stefano@gessa.net>
2024-07-30 23:50:30 +0700

    Add Storage module

---
+++ lib/ms2ex/storage.ex
@@ -0,0 +1,37 @@
+defmodule Ms2ex.Storage do
+  @ets_table :metadata
+  @max_age :timer.hours(1)
+
+  def get(key) do
+    case :ets.lookup(@ets_table, key) do
+      [{^key, {value, _}}] ->
+        {:ok, value}
+
+      [] ->
+        # TODO
+        value = Ms2ex.Metadata.get(key, key)
+        put(key, value)
+        {:ok, value}
+    end
+  end
+
+  def put(key, value) do
+    :ets.insert(@ets_table, {key, {value, :erlang.system_time(:millisecond)}})
+    :ok
+  end
+
+  def handle_info(:cleanup, state) do
+    current_time = :erlang.system_time(:millisecond)
+
+    :ets.select_delete(@ets_table, [
+      {{:"$1", {:"$2", :"$3"}}, [{:<, :"$3", current_time - @max_age}], [true]}
+    ])
+
+    schedule_cleanup()
+    {:noreply, state}
+  end
+
+  defp schedule_cleanup do
+    Process.send_after(self(), :cleanup, @max_age)
+  end
+end
--- lib/ms2ex/storage/additional_effect/additional_effect.ex
+++ lib/ms2ex/storage/additional_effect/additional_effect.ex
@@ -1,3 +1,2 @@
 defmodule Ms2ex.Storage.AdditionalEffect do
-  alias Ms2ex.Metadata
 end
