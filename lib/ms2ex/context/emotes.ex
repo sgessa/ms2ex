34d9ca8082bfb6a3d4d617e95d01ad729495964d
Stefano Gessa <stefano@gessa.net>
2025-06-17 04:15:41 +0700

    Add docs for Context.Emotes

--- lib/ms2ex/context/emotes.ex
+++ lib/ms2ex/context/emotes.ex
@@ -1,4 +1,11 @@
 defmodule Ms2ex.Context.Emotes do
+  @moduledoc """
+  Context module for character emote-related operations.
+
+  This module provides functions for listing, learning, and accessing
+  default emotes available to characters.
+  """
+
   alias Ms2ex.{Repo, Schema}
 
   import Ecto.Query, except: [update: 2]
@@ -11,6 +18,15 @@ defmodule Ms2ex.Context.Emotes do
     90_200_042
   ]
 
+  @doc """
+  Lists all emote IDs that a character has learned.
+
+  ## Examples
+
+      iex> list(character)
+      [90200011, 90200004, 90200024, 90200041, 90200042]
+  """
+  @spec list(Schema.Character.t()) :: [integer()]
   def list(%Schema.Character{id: character_id}) do
     Schema.Emote
     |> where([e], e.character_id == ^character_id)
@@ -18,6 +34,19 @@ defmodule Ms2ex.Context.Emotes do
     |> Repo.all()
   end
 
+  @doc """
+  Makes a character learn a new emote by ID.
+
+  ## Examples
+
+      iex> learn(character, 90200011)
+      {:ok, %Schema.Emote{}}
+
+      iex> learn(character, 90200011) # when already learned
+      {:error, %Ecto.Changeset{}}
+  """
+  @spec learn(Schema.Character.t(), integer()) ::
+          {:ok, Schema.Emote.t()} | {:error, Ecto.Changeset.t()}
   def learn(%Schema.Character{} = character, emote_id) do
     character
     |> Ecto.build_assoc(:emotes)
@@ -25,5 +54,14 @@ defmodule Ms2ex.Context.Emotes do
     |> Repo.insert()
   end
 
+  @doc """
+  Returns the list of default emote IDs available to all characters.
+
+  ## Examples
+
+      iex> default_emotes()
+      [90200011, 90200004, 90200024, 90200041, 90200042]
+  """
+  @spec default_emotes() :: [integer()]
   def default_emotes(), do: @default_emotes
 end
