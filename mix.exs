c6e0c0d07fc0ff6cb4b4cce8052a095dcea570a8
Stefano Gessa <stefano@gessa.net>
2025-06-17 02:37:36 +0700

    Use dotenv files for configuration

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Ms2ex.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:dotenvy, "~> 1.0.0"},
       {:phoenix, "~> 1.6"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.9"},
