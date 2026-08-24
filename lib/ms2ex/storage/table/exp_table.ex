3e951665eb9193b0d72afec41b7f21b3288b48ad
Stefano Gessa <stefano@bootando.com>
2026-08-24 09:01:58 +0700

    Rename exp table key from glob 'exp*.xml' to 'exp.xml'
    Upstream TableNames.EXP uses a glob because Maple2 merges several exp
    files into one record; we store a single document, so the asterisk was
    noise. Name overrides live in TableProjection; --drop-data now drops
    sets unconditionally so renamed keys don't linger.

--- lib/ms2ex/storage/table/exp_table.ex
+++ lib/ms2ex/storage/table/exp_table.ex
@@ -1,7 +1,7 @@
 defmodule Ms2ex.Storage.Tables.ExpTable do
   alias Ms2ex.Storage
 
-  @table_name "exp*.xml"
+  @table_name "exp.xml"
 
   @doc "Returns `{:ok, exp_to_next_level}` for a character level, or :error."
   @spec to_next_level(pos_integer()) :: {:ok, non_neg_integer()} | :error
