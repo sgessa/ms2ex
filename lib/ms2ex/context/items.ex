6563434233e14bcdaac57d56c17773169e0a2ec5
Stefano Gessa <stefano@bootando.com>
2026-08-24 14:14:24 +0700

    Fix inverted mesos?/1 clause order (always returned false)

--- lib/ms2ex/context/items.ex
+++ lib/ms2ex/context/items.ex
@@ -18,8 +18,8 @@ defmodule Ms2ex.Context.Items do
   end
 
   @meso_ids [90_000_001, 90_000_002, 90_000_003]
-  def mesos?(%Schema.Item{}), do: false
   def mesos?(%Schema.Item{item_id: id}) when id in @meso_ids, do: true
+  def mesos?(%Schema.Item{}), do: false
   def mesos(amount), do: init(List.first(@meso_ids), %{amount: amount})
 
   @meret_ids [90_000_004, 90_000_011, 90_000_015, 90_000_016]
