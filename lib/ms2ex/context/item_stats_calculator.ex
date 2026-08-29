89014b72365f3110acec22d33ee6ecef7b90d902
Alessandro Gessa <alessandro@gessa.net>
2026-08-29 02:55:41 +0200

    Refactor ItemStatsCalculator

--- lib/ms2ex/context/item_stats_calculator.ex
+++ lib/ms2ex/context/item_stats_calculator.ex
@@ -33,6 +33,19 @@ defmodule Ms2ex.Context.ItemStatsCalculator do
     perfect_guard: :static_rate_abp
   }
 
+  @allowed_constants [
+    :health,
+    :strength,
+    :dexterity,
+    :intelligence,
+    :luck,
+    :critical_rate,
+    :magical_atk,
+    :magical_res,
+    :physical_res
+  ]
+  @allowed_statics [:health, :physical_atk, :magical_atk, :physical_res, :magical_res]
+
   def constant_stats, do: @constant_stats
   def static_stats, do: @static_stats
 
@@ -45,118 +58,97 @@ defmodule Ms2ex.Context.ItemStatsCalculator do
   def constant_value(:max_weapon_atk, stat_value, deviation, item),
     do: weapon_attack_value(:max, stat_value, deviation, item)
 
-  def constant_value(:defense, stat_value, deviation, item) do
-    ItemDefense.constant(
-      stat_value,
-      deviation,
-      item_type(item),
-      first_recommended_job(item),
-      item.metadata.option.level_factor,
-      item.rarity,
-      item.metadata.limit.level
-    )
-  end
+  def constant_value(:defense, stat_value, deviation, item),
+    do:
+      ItemDefense.constant(
+        stat_value,
+        deviation,
+        item_type(item),
+        first_recommended_job(item),
+        item.metadata.option.level_factor,
+        item.rarity,
+        item.metadata.limit.level
+      )
 
-  def constant_value(stat, stat_value, deviation, item)
-      when stat in [
-             :health,
-             :strength,
-             :dexterity,
-             :intelligence,
-             :luck,
-             :critical_rate,
-             :magical_atk,
-             :magical_res,
-             :physical_res
-           ] do
-    ItemBasicStats.constant(
-      stat,
-      stat_value,
-      deviation,
-      %{
-        item_type: item_type(item),
-        job: first_recommended_job(item),
-        factor: item.metadata.option.level_factor,
-        grade: item.rarity,
-        level: item.metadata.limit.level,
-        p7: if(stat == :intelligence, do: 1, else: 0)
-      }
-    )
-  end
+  def constant_value(stat, stat_value, deviation, item) when stat in @allowed_constants,
+    do:
+      ItemBasicStats.constant(
+        stat,
+        stat_value,
+        deviation,
+        %{
+          item_type: item_type(item),
+          job: first_recommended_job(item),
+          factor: item.metadata.option.level_factor,
+          grade: item.rarity,
+          level: item.metadata.limit.level,
+          p7: if(stat == :intelligence, do: 1, else: 0)
+        }
+      )
 
-  def constant_value(stat, _stat_value, _deviation, _item),
-    do: raise(ArgumentError, "unsupported constant item stat: #{inspect(stat)}")
+  def static_value(:max_weapon_atk, stat_value, deviation, item),
+    do:
+      ItemWeaponAttack.static_value(
+        :max,
+        stat_value,
+        deviation,
+        item_type(item),
+        0,
+        item.metadata.option.level_factor,
+        item.rarity,
+        item.metadata.limit.level
+      )
+      |> roll_value()
 
-  def static_value(:max_weapon_atk, stat_value, deviation, item) do
-    item
-    |> static_weapon_attack_value(stat_value, deviation)
-    |> roll_value()
-  end
-
-  def static_value(:perfect_guard, stat_value, _deviation, item) do
-    ItemRates.static_perfect_guard(stat_value, item.metadata.option.level_factor)
-    |> roll_value()
-  end
+  def static_value(:perfect_guard, stat_value, _deviation, item),
+    do:
+      ItemRates.static_perfect_guard(stat_value, item.metadata.option.level_factor)
+      |> roll_value()
 
   def static_value(stat, stat_value, _deviation, item)
-      when stat in [:health, :physical_atk, :magical_atk, :physical_res, :magical_res] do
-    ItemStaticStats.value(stat, stat_value, item.metadata.option.level_factor, item_type(item))
-    |> roll_value()
-  end
+      when stat in @allowed_statics,
+      do:
+        stat
+        |> ItemStaticStats.value(stat_value, item.metadata.option.level_factor, item_type(item))
+        |> roll_value()
 
-  def static_value(:defense, stat_value, _deviation, item) do
-    ItemDefense.static(
-      stat_value,
-      item_type(item),
-      first_recommended_job(item),
-      item.metadata.option.level_factor,
-      item.rarity,
-      item.metadata.limit.level
-    )
-    |> roll_value()
-  end
+  def static_value(:defense, stat_value, _deviation, item),
+    do:
+      ItemDefense.static(
+        stat_value,
+        item_type(item),
+        first_recommended_job(item),
+        item.metadata.option.level_factor,
+        item.rarity,
+        item.metadata.limit.level
+      )
+      |> roll_value()
 
-  def static_value(stat, _stat_value, _deviation, _item),
-    do: raise(ArgumentError, "unsupported static item stat: #{inspect(stat)}")
-
-  defp weapon_attack_value(bound, stat_value, deviation, item) do
-    ItemWeaponAttack.constant_value(
-      bound,
-      stat_value,
-      deviation,
-      item_type(item),
-      0,
-      item.metadata.option.level_factor,
-      item.rarity,
-      item.metadata.limit.level
-    )
-  end
-
-  defp static_weapon_attack_value(item, stat_value, deviation) do
-    ItemWeaponAttack.static_value(
-      :max,
-      stat_value,
-      deviation,
-      item_type(item),
-      0,
-      item.metadata.option.level_factor,
-      item.rarity,
-      item.metadata.limit.level
-    )
-  end
+  defp weapon_attack_value(bound, stat_value, deviation, item),
+    do:
+      ItemWeaponAttack.constant_value(
+        bound,
+        stat_value,
+        deviation,
+        item_type(item),
+        0,
+        item.metadata.option.level_factor,
+        item.rarity,
+        item.metadata.limit.level
+      )
 
   defp roll_value({minimum, maximum}),
     do: minimum + (maximum + 1 - minimum) * :rand.uniform()
 
-  defp first_recommended_job(item) do
-    item.metadata.limit
-    |> Map.get(:job_limits, [])
-    |> List.first() || 0
-  end
+  defp first_recommended_job(item),
+    do:
+      item.metadata.limit
+      |> Map.get(:job_limits, [])
+      |> List.first() || 0
 
-  defp item_type(item) do
-    item.item_id
-    |> Context.ItemTypes.get_type_by_item_id()
-    |> Enums.ItemType.get_value()
-  end
+  defp item_type(item),
+    do:
+      item.item_id
+      |> Context.ItemTypes.get_type_by_item_id()
+      |> Enums.ItemType.get_value()
 end
