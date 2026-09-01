11bd5b7f6aee1fa95f00d7fbc1e940e4f7d240a0
Alessandro Gessa <alessandro@gessa.net>
2026-09-01 15:17:25 +0200

    Fix: group chat invite

--- lib/ms2ex/handlers/game/group_chat.ex
+++ lib/ms2ex/handlers/game/group_chat.ex
@@ -15,10 +15,11 @@ defmodule Ms2ex.GameHandlers.GroupChat do
     handle_mode(mode, packet, session)
   end
 
-  # Create
-  def handle_mode(0x1, _packet, session) do
+  # Create (optionally with an immediate invite)
+  def handle_mode(0x1, packet, session) do
+    {rcpt_name, _packet} = get_ustring(packet)
     {:ok, character} = Managers.Character.call(session.character_id, :lookup)
-    maybe_create_chat(session, character)
+    maybe_create_chat(session, character, rcpt_name)
   end
 
   # Invite
@@ -72,12 +73,12 @@ defmodule Ms2ex.GameHandlers.GroupChat do
     end
   end
 
-  defp maybe_create_chat(session, %{group_chat_ids: ids})
+  defp maybe_create_chat(session, %{group_chat_ids: ids}, _rcpt_name)
        when length(ids) >= @max_chats_per_user do
     session
   end
 
-  defp maybe_create_chat(session, character) do
+  defp maybe_create_chat(session, character, rcpt_name) do
     chat = %GroupChat{id: Ms2ex.generate_int(), member_ids: [character.id]}
     {:ok, _} = GroupChat.start(chat)
 
@@ -86,9 +87,32 @@ defmodule Ms2ex.GameHandlers.GroupChat do
     ids = [chat.id | character.group_chat_ids]
     Managers.Character.call(character, {:update, %{character | group_chat_ids: ids}})
 
-    session
-    |> push(Packets.GroupChat.update(%{chat | members: [character]}))
-    |> push(Packets.GroupChat.create(chat))
+    session =
+      session
+      |> push(Packets.GroupChat.update(%{chat | members: [character]}))
+      |> push(Packets.GroupChat.create(chat))
+
+    if rcpt_name != "" do
+      maybe_invite_to_new_chat(session, character, chat, rcpt_name)
+    else
+      session
+    end
+  end
+
+  defp maybe_invite_to_new_chat(session, character, chat, rcpt_name) do
+    with {:ok, rcpt} <- get_rcpt(character, rcpt_name),
+         :ok <- validate_rcpt(character, rcpt) do
+      ids = [chat.id | rcpt.group_chat_ids]
+      Managers.Character.call(rcpt, {:update, %{rcpt | group_chat_ids: ids}})
+
+      {:ok, chat} = GroupChat.add_member(chat, rcpt)
+      GroupChat.broadcast(chat.id, Packets.GroupChat.update_members(chat, rcpt))
+
+      send(rcpt.sender_session_pid, {:join_group_chat, character, rcpt, chat})
+      push(session, Packets.GroupChat.invite(character, rcpt, chat))
+    else
+      _ -> session
+    end
   end
 
   defp get_chat(member, chat_id) do
