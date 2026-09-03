5d96a0e1ee751922b4cd9df1ef89650e8e1919ea
Alessandro Gessa <alessandro@gessa.net>
2026-09-03 21:37:22 +0200

    Rewrite calcEnchantStats.lua

--- lib/ms2ex/context/item_enchant_stats.ex
+++ lib/ms2ex/context/item_enchant_stats.ex
@@ -1,12 +1,12 @@
 defmodule Ms2ex.Context.ItemEnchantStats do
+  alias Ms2ex.Context
   alias Ms2ex.Enums
-  alias Ms2ex.Lua.ItemEnchant
+  alias Ms2ex.Formulas.ItemEnchant
   alias Ms2ex.Schema
   alias Ms2ex.Types
 
   def get(%Schema.Item{} = item) do
-    item
-    |> ItemEnchant.get_values()
+    ItemEnchant.values(item.enchant_level, item_type(item), item.metadata.limit.level)
     |> Enum.chunk_every(2)
     |> Enum.reduce(%{}, fn [attr_nr, value], acc ->
       if attr_nr == 0 do
@@ -18,4 +18,10 @@ defmodule Ms2ex.Context.ItemEnchantStats do
       end
     end)
   end
+
+  defp item_type(item),
+    do:
+      item.item_id
+      |> Context.ItemTypes.get_type_by_item_id()
+      |> Enums.ItemType.get_value()
 end
